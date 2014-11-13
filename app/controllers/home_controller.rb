cb1dd392e423b5cebc52d56e105fc54681abc918
Yuval Dvir <yuval.dvir@naturalint.com>
2014-11-13 23:11:49 +0000

    added all cominations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,28 +22,66 @@ class HomeController < ApplicationController
     tables_with_one_reference = []
 
     all_tables = {}
+    
+    all_possible_embedded = []
 
     @translation.tables.each do |table|
       all_tables[table.name] = table
       number_of_referece_columns = 0
       table.columns.each do |column|
         number_of_referece_columns += 1 if column.references
+        all_possible_embedded << {table: table.name, column:column.name}
       end
       tables_with_one_reference << table if number_of_referece_columns == 1
     end
+    length = all_possible_embedded.length
+    all_reference_combinations = []
+    all_reference_combinations << 0.upto(length) { |n| tables_with_one_reference.combination(n) }.flatten
 
-    tables_with_one_reference.each do |table|
-      table.columns.each do |column|
-        if(column.references)
+    # tables_with_one_reference.each do |table|
+    #   table.columns.each do |column|
+    #     if(column.references)
+    #       referenced_table_name = column.references
+    #       table.options['embed_in'] = referenced_table_name
+    #     end
+    #   end
+    # end
+    
+    
+    
+    
+   all_reference_combinations.each do |reference_combination|
+     @translation = Mongify::Translation.load(@configuration.sql_connection)
+     all_tables = ge_all_tables_hash
+     reference_combination.each do |referenced_column|
+       table_name = referenced_column[:table]
+       table = all_tables[table_name]
+       column_name = referenced_column[:column]
+       table.columns.each do |column|
+        if(column.name == column_name)
           referenced_table_name = column.references
           table.options['embed_in'] = referenced_table_name
         end
-      end
+       end
+     end
     end
+    
+     translation_string = @translation.print
+     
+     puts translation_string
+    
 
-    @translation.process(@configuration.sql_connection, @configuration.no_sql_connection)
+    #@translation.process(@configuration.sql_connection, @configuration.no_sql_connection)
 
 
+  end
+  
+  def ge_all_tables_hash
+    all_tables = {}
+    @translation.tables.each do |table|
+      all_tables[table.name] = table
+    end
+    all_tables
   end
 
   def check_sql_connection
